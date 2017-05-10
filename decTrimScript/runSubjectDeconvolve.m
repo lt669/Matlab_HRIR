09341c1e1556e41b3b4b1b3874037e5fe19ed8c4
Lewis Thresh <lt669@york.ac.uk>
2017-05-10 17:15:10 +0100

    Added error check to file path. Added variable trim to start of IRs depending on FS. (This value needs reevaluating)

--- decTrimScript/runSubjectDeconvolve.m
+++ decTrimScript/runSubjectDeconvolve.m
@@ -2,21 +2,29 @@
     Deconvoles and trims the HRIRs of each of the test subjects
 %}
 
-function [decStereoOut] = runSubjectDeconvolve(projectName,subjectName,fileLength,fs,bitDepth)
+function [decStereoOut] = runSubjectDeconvolve(projectName,subjectName,fileLength,fs,bits)
 
     disp('--- Running Subject Deconvolve Script ---');
 
     % Variables
     if fs==48
         samples = 144000;
+        trimStart = 2414;
     elseif fs==44
         samples = 132300;
+        trimStart = 2218;
     end
     
     % Paths to audio files
     subjectSweepsPath = sprintf('Audio/%s/SubjectSweeps_Raw/%s/%i',projectName,subjectName,fs); % Used to load files
     subjectDir = dir(sprintf('%s/*.wav',subjectSweepsPath)); % Used to find names of files
 
+    % Check file exists
+    if 0==exist(subjectSweepsPath,'file')
+        error(sprintf('The Folder... \n\n %s \n\n ...does not exists. Have you selected the correct sampling rate?',subjectSweepsPath));
+    end
+
+    
     %Load inverse sweep
     if fs==48
         inv = audioread('Audio/Sweeps/InvSweep_20to22050_48000_startPad0s_endPad0s.wav');
@@ -75,23 +83,28 @@ function [decStereoOut] = runSubjectDeconvolve(projectName,subjectName,fileLengt
 
     for n = 1:length(subjectDir)
 
-        % Check for sample rate conversion
-        if(fs == 44100)
-
-        end
-
         % Write Raw HRIR
         outputRaw(:,:) = decNorm(n,:,:);
-        name = strcat('Audio/HRIR_Raw/',subjectName,'/',char(fileName(n)),'_RawLong.wav');
-        audiowrite(name,outputRaw,fs);
+        name = strcat('Audio/',projectName,'/HRIR_Raw/',subjectName,'/',fs,'/',char(fileName(n)),'_RawLong.wav');
+        audiowrite(name,outputRaw,fs,'BitsPerSample', bits);
 
+        %{
+            To cut 0.05s off start of the IR
+        
+            48k = 2414 (This was the original value used in EAD)
+            44.1k = 2218 (This is approximate equivalent to the previously
+            used value)
+        
+            This value should be better determined.
+        %}
+        
         % Write Trimmed HRIR
-        decStereoOut(n,:,1) = decNorm(n,2414:(2414+fileLength-1),1);
-        decStereoOut(n,:,2) = decNorm(n,2414:(2414+fileLength-1),2);
-        name = strcat('Audio/HRIR_Trim/',subjectName,'/',char(fileName(n)),'_Raw.wav');
+        decStereoOut(n,:,1) = decNorm(n,trimStart:(trimStart+fileLength-1),1); % What number should this be for 44.1k??
+        decStereoOut(n,:,2) = decNorm(n,trimStart:(trimStart+fileLength-1),2);
+        name = strcat('Audio/HRIR_Trim/',subjectName,'/',fs,'/',char(fileName(n)),'_Raw.wav');
 
         output(:,:) = decStereoOut(n,:,:);
-        audiowrite(name,output,fs,'BitsPerSample', 24);
+        audiowrite(name,output,fs,'BitsPerSample', bits);
     end
 
 
